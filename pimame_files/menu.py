2948d7a3d2e30a86ab1c1bde09ada2e28e985a19
Shea <shea.silverman@ucf.edu>
2013-06-14 22:33:08 -0300

    More games MESS DGen and Cavestory

--- pimame_files/menu.py
+++ pimame_files/menu.py
@@ -37,10 +37,13 @@ menu_data = {
   'title': "PiMAME Menu", 'type': MENU, 'subtitle':  "Please select an option...",
   'options': [
     { 'title': "AdvanceMAME", 'type': COMMAND, 'command': 'advmenu' },
+    { 'title': "AdvanceMESS", 'type': COMMAND, 'command': 'advmenu -cfg advmenu-mess.rc' },
     { 'title': "Neo Geo (GNGeo)", 'type': COMMAND, 'command': 'gngeo -i roms/' },
     { 'title': "PlayStation 1 (PCSX_ReARMed)", 'type': COMMAND, 'command': '/home/pi/emulators/pcsx_rearmed/pcsx' },
     { 'title': "MAME4All", 'type': COMMAND, 'command': '/home/pi/emulators/mame4all-pi/mame' },
-    { 'title': "SNES", 'type': COMMAND, 'command': '/home/pi/emulators/pisnes/snes9x.gui' },
+    { 'title': "SNES (PiSNES / SNES9x)", 'type': COMMAND, 'command': '/home/pi/emulators/pisnes/snes9x.gui' },
+    { 'title': "Genesis (DGen)", 'type': COMMAND, 'command': 'advmenu -cfg advmenu-dgen.rc' },
+    { 'title': "CaveStory (NXEngine)", 'type': COMMAND, 'command': '/home/pi/emulators/nxengine/nx' },
     { 'title': "Tools", 'type': MENU, 'subtitle': myip,
     'options': [
       { 'title': "Install PIP (http://pip.sheacob.com/about.html)", 'type': COMMAND, 'command': 'sudo /home/pi/pimame_files/pipinstall.py' },
